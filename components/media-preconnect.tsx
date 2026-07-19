d05ac559b4d6d69f907efd84d602a643cf70e420
User <user@example.com>
2026-07-19 10:41:54 +0400

    fix(lint): move URL parsing out of JSX try/catch in MediaPreconnect

--- components/media-preconnect.tsx
+++ components/media-preconnect.tsx
@@ -1,18 +1,23 @@
 'use client';
 
-export function MediaPreconnect() {
+function getMediaOrigin(): string | null {
   const base = process.env.NEXT_PUBLIC_MEDIA_BASE_URL;
   if (!base) return null;
-  
   try {
-    const origin = new URL(base).origin;
-    return (
-      <>
-        <link rel="preconnect" href={origin} crossOrigin="anonymous" />
-        <link rel="dns-prefetch" href={origin} />
-      </>
-    );
+    return new URL(base).origin;
   } catch {
     return null;
   }
+}
+
+const origin = getMediaOrigin();
+
+export function MediaPreconnect() {
+  if (!origin) return null;
+  return (
+    <>
+      <link rel="preconnect" href={origin} crossOrigin="anonymous" />
+      <link rel="dns-prefetch" href={origin} />
+    </>
+  );
 }
